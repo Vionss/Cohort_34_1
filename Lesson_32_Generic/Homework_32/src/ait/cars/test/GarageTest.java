a0a2c344ce26ea44e2b9e7c43f11a744055fa070
vions <vions323@gmail.com>
2023-10-19 16:30:05 +0200

    lesson 22

---
+++ Lesson_32_Generic/Homework_32/src/ait/cars/test/GarageTest.java
@@ -0,0 +1,38 @@
+package ait.cars.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GarageTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addCar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeCar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findCarByRegNumber() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findCarsByModel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findCarsByCompany() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findCarsByEngine() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findCarsByColor() {
+    }
+}
